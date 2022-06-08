04337c08c7d7d574fe992f8e0736ed4551630433
alexandr74736 <a17230@yandex.ru>
2022-06-08 06:57:36 +0400

    Updated modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,21 +1,29 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { LoginPageComponent } from "./login-page/login-page.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AdminLayoutComponent } from "./shared/components/admin-layout/admin-layout.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import { AuthService } from "./shared/services/auth.service";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
         AdminLayoutComponent,
+        LoginPageComponent,
         DashboardPageComponent,
         CreatePageComponent,
         EditPageComponent
     ],
     imports: [
         CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule,
         RouterModule.forChild( [
             {
                 path: '', component: AdminLayoutComponent, children: [
@@ -30,6 +38,7 @@ import { EditPageComponent } from './edit-page/edit-page.component';
         ])
     ],
     exports: [RouterModule],
+    providers: [AuthService]
 })
 export class AdminModule {
 
